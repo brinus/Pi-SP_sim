f16f8f60a236061bf97019b13c8e9db696d53fe3
Matteo Brini <brinimatteo@gmail.com>
2022-09-22 15:15:06 +0200

    code rearrangement, added detector command from GUI

--- include/construct.hh
+++ include/construct.hh
@@ -8,6 +8,7 @@
 #include "G4NistManager.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4PhysicalConstants.hh"
+#include "G4GenericMessenger.hh"
 
 #include "G4Box.hh"
 #include "G4Tubs.hh"
@@ -24,9 +25,23 @@ class MyDetectorConstruction : public G4VUserDetectorConstruction
         virtual G4VPhysicalVolume * Construct();
 
     private:
-        G4LogicalVolume * logicFDet;
-        G4LogicalVolume * logicLH2;
+        G4Box *solidWorld;
+        G4Tubs *solidCyl_o, *solidCyl_v, *solidCyl_i, *solidLH2, *solidFDet, *solidThick;
+        G4LogicalVolume *logicWorld, *logicCyl_o, *logicCyl_v, *logicCyl_i, *logicLH2, *logicFDet, *logicThick;
+        G4VPhysicalVolume *physWorld, *physCyl_o, *physCyl_v, *physCyl_i, *physLH2, *physFDet, *physThick;
+
+        G4double xWorld, yWorld, zWorld;
+
+        G4Material *worldMat, *lH2, *inox, *vacuum;
+
+        G4GenericMessenger * fMessenger;
+
+        G4int layers;
+        G4double thick;
+
         virtual void ConstructSDandField();
+        void DefineMaterial();
+
 };
 
 #endif
--- src/construct.cc
+++ src/construct.cc
@@ -1,17 +1,44 @@
 #include "construct.hh"
 
 MyDetectorConstruction::MyDetectorConstruction()
-{}
+{
+    fMessenger = new G4GenericMessenger(this, "/detector/", "Detector construction");
+    fMessenger->DeclareProperty("layers", layers, "Number of layers before entering the cylinder");
+    fMessenger->DeclareProperty("thick", thick, "Thickness of a single layer");
+
+    layers = 10;
+    thick = 0.5;
+
+    xWorld = 50*km;
+    yWorld = 50*km;
+    zWorld = 50*km;
+
+    DefineMaterial();
+}
 
 MyDetectorConstruction::~MyDetectorConstruction()
 {}
 
+void MyDetectorConstruction::DefineMaterial()
+{
+    // ---------- MATERIALS -------------------------------
+
+    G4NistManager * nist = G4NistManager::Instance();
+    G4double density;
+
+    worldMat = nist->FindOrBuildMaterial("G4_AIR");
+
+    lH2 = new G4Material("Liquid hidrogen", density = 0.07085*g/cm3, 1, kStateLiquid);
+    lH2->AddElement(nist->FindOrBuildElement("H"), 2);
+
+    inox = nist->FindOrBuildMaterial("G4_STAINLESS-STEEL");
+
+    vacuum = nist->FindOrBuildMaterial("G4_Galactic");
+}
+
 G4VPhysicalVolume * MyDetectorConstruction::Construct()
 {
     // ---------- VARIABLES -------------------------------
-    
-    G4NistManager * nist = G4NistManager::Instance();
-    G4double density;
 
     G4VisAttributes * cGrey = new G4VisAttributes(G4Colour(0.8, 0.8, 0.8));
     cGrey->SetForceWireframe(true);
@@ -28,67 +55,54 @@ G4VPhysicalVolume * MyDetectorConstruction::Construct()
     G4VisAttributes * cYellow = new G4VisAttributes(G4Colour(1, 1, 0));
     cYellow->SetForceWireframe(true);
 
-    // ---------- MATERIALS -------------------------------
-
-    G4Material * worldMat = nist->FindOrBuildMaterial("G4_AIR");
-
-    G4Material * lH2 = new G4Material("Liquid hidrogen", density = 0.07085*g/cm3, 1, kStateLiquid);
-    lH2->AddElement(nist->FindOrBuildElement("H"), 2);
-
-    G4Material * inox = nist->FindOrBuildMaterial("G4_STAINLESS-STEEL");
-
-    G4Material * vacuum = nist->FindOrBuildMaterial("G4_Galactic");
-
     // ---------- OBJECTS ---------------------------------
 
     // World
-    G4Box * solidWorld = new G4Box("solidWorld", 0.2*m, 0.2*m, 0.2*m);
-    G4LogicalVolume * logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
-    G4VPhysicalVolume * physWorld = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicWorld, "physWorld", 0, false, 0, false);
+    solidWorld = new G4Box("solidWorld", xWorld, yWorld, zWorld);
+    logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
+    physWorld = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicWorld, "physWorld", 0, false, 0, false);
 
     // Outern cylinder
-    G4Tubs * solidCyl_o = new G4Tubs("solidCyl_o", 0.*mm, 53*mm, 55*mm, 0, twopi);
-    G4LogicalVolume * logicCyl_o = new G4LogicalVolume(solidCyl_o, inox, "logicCyl_o");
-    G4VPhysicalVolume * physCyl_o = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicCyl_o, "physCyl_o", logicWorld, false, 0, true);
+    solidCyl_o = new G4Tubs("solidCyl_o", 0.*mm, 53*mm, 55*mm, 0, twopi);
+    logicCyl_o = new G4LogicalVolume(solidCyl_o, inox, "logicCyl_o");
+    physCyl_o = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicCyl_o, "physCyl_o", logicWorld, false, 0, true);
 
     logicCyl_o->SetVisAttributes(cGrey);
 
     // Vacuum cylinder
-    G4Tubs * solidCyl_v = new G4Tubs("solidCyl_v", 0.*mm, 50*mm, 54.5*mm, 0, twopi);
-    G4LogicalVolume * logicCyl_v = new G4LogicalVolume(solidCyl_v, vacuum, "logicCyl_v");
-    G4VPhysicalVolume * physCyl_v = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicCyl_v, "physCyl_v", logicCyl_o, false, 0, true);
+    solidCyl_v = new G4Tubs("solidCyl_v", 0.*mm, 50*mm, 54.5*mm, 0, twopi);
+    logicCyl_v = new G4LogicalVolume(solidCyl_v, vacuum, "logicCyl_v");
+    physCyl_v = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicCyl_v, "physCyl_v", logicCyl_o, false, 0, true);
 
     logicCyl_v->SetVisAttributes(cGrey);
 
     // Inner cylinder
-    G4Tubs * solidCyl_i = new G4Tubs("solidCyl_i", 0.*mm, 30*mm, 35*mm, 0, twopi);
-    G4LogicalVolume * logicCyl_i = new G4LogicalVolume(solidCyl_i, inox, "logicCyl_i");
-    G4VPhysicalVolume * physCyl_i = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicCyl_i, "physCyl_i", logicCyl_v, false, 0, true);
+    solidCyl_i = new G4Tubs("solidCyl_i", 0.*mm, 30*mm, 35*mm, 0, twopi);
+    logicCyl_i = new G4LogicalVolume(solidCyl_i, inox, "logicCyl_i");
+    physCyl_i = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicCyl_i, "physCyl_i", logicCyl_v, false, 0, true);
 
     logicCyl_i->SetVisAttributes(cCyan_l);
 
     // LH2 cylinder
-    G4Tubs * solidLH2 = new G4Tubs("solidLH2", 0.*mm, 29.5*mm, 34.5*mm, 0, twopi);
+    solidLH2 = new G4Tubs("solidLH2", 0.*mm, 29.5*mm, 34.5*mm, 0, twopi);
     logicLH2 = new G4LogicalVolume(solidLH2, lH2, "logicLH2");
-    G4VPhysicalVolume * physLH2 = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicLH2, "physLH2", logicCyl_i, false, 0, true);
+    physLH2 = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicLH2, "physLH2", logicCyl_i, false, 0, true);
 
     logicLH2->SetVisAttributes(cCyan_l);
 
     // First detector (FDet)
-    G4Tubs * solidFDet = new G4Tubs("solidFDet", 0.*mm, 49.9*mm, 0.5*mm, 0, twopi);
+    solidFDet = new G4Tubs("solidFDet", 0.*mm, 49.9*mm, 0.5*mm, 0, twopi);
     logicFDet = new G4LogicalVolume(solidFDet, vacuum, "logicFDet");
-    G4VPhysicalVolume * physFDet = new G4PVPlacement(0, G4ThreeVector(0, 0, -54*mm), logicFDet, "physFDet", logicCyl_v, false, 0, true);  
+    physFDet = new G4PVPlacement(0, G4ThreeVector(0, 0, -54*mm), logicFDet, "physFDet", logicCyl_v, false, 0, true);  
 
     logicFDet->SetVisAttributes(cYellow);
 
     // Modulable thickness layer
-    double thick = 0.05;
-    G4Tubs * solidThick = new G4Tubs("solidThick", 0.*mm, 53.*mm, (thick/2)*mm, 0, twopi);
-    G4LogicalVolume * logicThick = new G4LogicalVolume(solidThick, inox, "logicThick");
+    solidThick = new G4Tubs("solidThick", 0.*mm, 53.*mm, (thick/2)*mm, 0, twopi);
+    logicThick = new G4LogicalVolume(solidThick, inox, "logicThick");
 
-    int Layers = 20;
-    for(int i=0; i<Layers; i++){
-        G4VPhysicalVolume * physThick = new G4PVPlacement(0, G4ThreeVector(0, 0, (-55 - thick/2 - thick*i)*mm), logicThick, "physThick", logicWorld, false, i, true);
+    for(int i=0; i<layers; i++){
+        physThick = new G4PVPlacement(0, G4ThreeVector(0, 0, (-55. - thick/2 - thick*i)*mm), logicThick, "physThick", logicWorld, false, i, true);
     }
 
     logicThick->SetVisAttributes(cGreen);
