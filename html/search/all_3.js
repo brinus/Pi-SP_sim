985affbc1cf612eede483232066391f7b37ab7ae
Matteo Brini <brinimatteo@gmail.com>
2022-09-26 15:17:47 +0200

    docs commit

---
+++ html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['set_5faxes_5fequal_0',['set_axes_equal',['../namespaceplot__3d.html#a1c7d5c86680fe42e02d73d97a36e4025',1,'plot_3d']]]
+];
---
+++ html/search/namespaces_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['plot_5f3d_0',['plot_3d',['../namespaceplot__3d.html',1,'']]]
+];
