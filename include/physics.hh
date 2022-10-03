1111c7154038e65caaec7e4c78fe3913637a3395
Matteo Brini <brinimatteo@gmail.com>
2022-10-03 14:06:16 +0200

    implemented custom physics list (core dump)

--- include/physics.hh
+++ include/physics.hh
@@ -7,13 +7,44 @@
 #include "G4DecayPhysics.hh"
 #include "G4OpticalPhysics.hh"
 #include "G4HadronPhysicsFTFP_BERT.hh"
+#include "G4VUserPhysicsList.hh"
 
+#include "G4PionMinus.hh"
+#include "G4PionZero.hh"
+#include "G4Proton.hh"
+#include "G4Neutron.hh"
+#include "G4Gamma.hh"
+
+#include "G4MesonConstructor.hh"
+#include "G4BosonConstructor.hh"
+#include "G4BaryonConstructor.hh"
+
+#include "G4ChargeExchangeProcess.hh"
+#include "G4Decay.hh"
 
 class MyPhysicsList : public G4VModularPhysicsList
 {
-    public:
-        MyPhysicsList();
-        ~MyPhysicsList();
+public:
+    MyPhysicsList();
+    ~MyPhysicsList();
+};
+
+class MyUserPhysicsList : public G4VUserPhysicsList
+{
+public:
+    MyUserPhysicsList();
+    ~MyUserPhysicsList();
+
+    virtual void ConstructParticle();
+    virtual void ConstructEM();
+    virtual void ConstructProcess();
+
+    void ContructGeneral();
+
+private:
+    G4MesonConstructor mConstructor;
+    G4BosonConstructor boConstructor;
+    G4BaryonConstructor baConstructor;
 };
 
 #endif
--- src/physics.cc
+++ src/physics.cc
@@ -2,12 +2,87 @@
 
 MyPhysicsList::MyPhysicsList()
 {
-    RegisterPhysics ( new G4EmStandardPhysics());
-    RegisterPhysics ( new G4ChargeExchangePhysics());
-    RegisterPhysics ( new G4DecayPhysics());
-    RegisterPhysics ( new G4HadronPhysicsFTFP_BERT());
-    RegisterPhysics ( new G4OpticalPhysics());
+    RegisterPhysics(new G4EmStandardPhysics());
+    RegisterPhysics(new G4ChargeExchangePhysics());
+    RegisterPhysics(new G4DecayPhysics());
+    RegisterPhysics(new G4HadronPhysicsFTFP_BERT());
+    RegisterPhysics(new G4OpticalPhysics());
 }
 
 MyPhysicsList::~MyPhysicsList()
-{}
+{
+}
+
+MyUserPhysicsList::MyUserPhysicsList()
+{
+    //ConstructParticle();
+    //ConstructProcess();
+
+    // RegisterPhysics(new G4EmStandardPhysics());
+}
+
+MyUserPhysicsList::~MyUserPhysicsList()
+{
+}
+
+void MyUserPhysicsList::ConstructParticle()
+{
+    mConstructor.ConstructParticle();
+    boConstructor.ConstructParticle();
+    baConstructor.ConstructParticle();
+    G4Gamma::Definition();
+}
+
+void MyUserPhysicsList::ConstructProcess()
+{
+    AddTransportation();
+    ConstructEM();
+    ContructGeneral();
+}
+
+void MyUserPhysicsList::ConstructEM()
+{
+    G4PhysicsListHelper *ph = G4PhysicsListHelper::GetPhysicsListHelper();
+    auto particleIterator = GetParticleIterator();
+    particleIterator->reset();
+    while ((*particleIterator)())
+    {
+        G4ParticleDefinition *particleDefinition = particleIterator->value();
+        if (particleDefinition == G4Gamma::Definition())
+        {
+            ph->RegisterProcess(new G4Decay(), particleDefinition);
+        }
+    }
+}
+
+void MyUserPhysicsList::ContructGeneral()
+{
+    G4PhysicsListHelper *ph = G4PhysicsListHelper::GetPhysicsListHelper();
+    auto particleIterator = GetParticleIterator();
+    particleIterator->reset();
+    while ((*particleIterator)())
+    {
+        G4ParticleDefinition *particleDefinition = particleIterator->value();
+        if (particleDefinition == G4PionMinus::Definition())
+        {
+            ph->RegisterProcess(new G4ChargeExchangeProcess(), particleDefinition);
+        }
+        else if (particleDefinition == G4PionZero::Definition())
+        {
+            ph->RegisterProcess(new G4ChargeExchangeProcess(), particleDefinition);
+            ph->RegisterProcess(new G4Decay(), particleDefinition);
+        }
+        else if (particleDefinition == G4Proton::Definition())
+        {
+            ph->RegisterProcess(new G4ChargeExchangeProcess(), particleDefinition);
+        }
+        else if (particleDefinition == G4Neutron::Definition())
+        {
+            ph->RegisterProcess(new G4ChargeExchangeProcess(), particleDefinition);
+        }
+        // else if (particleDefinition == G4Gamma::Definition())
+        // {
+        //     ph->RegisterProcess(new G4Decay(), particleDefinition);
+        // }
+    }
+}
